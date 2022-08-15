fb7b483f68da0fa45c6df69cf5d2ffe97ed281c4
Raphael Coelho <raphael_coelho@hotmail.com.br>
2022-08-15 17:45:09 -0300

    change: all set + header component

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head"
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+      <h1>Home</h1>
+    </>
+  )
+}
+export default Home
