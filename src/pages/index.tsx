d97b3579e695eff4f9b7d4e21bbf16a1fcf04eb3
Raphael Coelho <raphael_coelho@hotmail.com.br>
2022-08-15 20:21:18 -0300

    change: server side props to static

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next"
+import { GetStaticProps } from "next"
 import Head from "next/head"
 import { SubscribeButton } from "../components/SubscribeButton"
 import { stripe } from "../services/stripe"
@@ -34,7 +34,7 @@ const Home = ({ product }: HomeProps) => {
 }
 export default Home
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1LXBnKCQs03RhosuyNAr5YMB")
 
   const product = {
@@ -45,6 +45,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24 // 24 hours
   }
 }
