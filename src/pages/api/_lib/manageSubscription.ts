2f813855d1cb3bb4ec3934dd8c2d25461853525d
Raphael Coelho <raphael_coelho@hotmail.com.br>
2022-08-19 15:09:40 -0300

    feat: add subscription updated and deleted webhooks

--- src/pages/api/_lib/manageSubscription.ts
+++ src/pages/api/_lib/manageSubscription.ts
@@ -2,9 +2,7 @@ import { fauna } from "../../../services/fauna"
 import { query as q } from "faunadb"
 import { stripe } from "../../../services/stripe"
 
-export const saveSubscription = async (subscriptionId: string, customerId: string) => {
-  console.log(subscriptionId, customerId)
-
+export const saveSubscription = async (subscriptionId: string, customerId: string, createAction = false) => {
   const userRef = await fauna.query(q.Select("ref", q.Get(q.Match(q.Index("userByStripeCustomerId"), customerId))))
 
   const subscription = await stripe.subscriptions.retrieve(subscriptionId)
@@ -15,5 +13,14 @@ export const saveSubscription = async (subscriptionId: string, customerId: strin
     status: subscription.status,
     price_id: subscription.items.data[0].price.id
   }
-  await fauna.query(q.Create(q.Collection("subscriptions"), { data: subscriptionData }))
+
+  if (createAction) {
+    await fauna.query(q.Create(q.Collection("subscriptions"), { data: subscriptionData }))
+  } else {
+    await fauna.query(
+      q.Replace(q.Select("ref", q.Get(q.Match(q.Index("subscriptionById"), subscription.id))), {
+        data: subscriptionData
+      })
+    )
+  }
 }
