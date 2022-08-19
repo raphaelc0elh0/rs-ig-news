52e64c9e2e392351ca8bd4a239cfc123e2f5ecc0
Raphael Coelho <raphael_coelho@hotmail.com.br>
2022-08-19 14:53:36 -0300

    feat: add subscriptions webhook and management

---
+++ src/pages/api/_lib/manageSubscription.ts
@@ -0,0 +1,19 @@
+import { fauna } from "../../../services/fauna"
+import { query as q } from "faunadb"
+import { stripe } from "../../../services/stripe"
+
+export const saveSubscription = async (subscriptionId: string, customerId: string) => {
+  console.log(subscriptionId, customerId)
+
+  const userRef = await fauna.query(q.Select("ref", q.Get(q.Match(q.Index("userByStripeCustomerId"), customerId))))
+
+  const subscription = await stripe.subscriptions.retrieve(subscriptionId)
+
+  const subscriptionData = {
+    id: subscription.id,
+    userId: userRef,
+    status: subscription.status,
+    price_id: subscription.items.data[0].price.id
+  }
+  await fauna.query(q.Create(q.Collection("subscriptions"), { data: subscriptionData }))
+}
