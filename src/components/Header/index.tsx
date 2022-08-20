2b228b02b82285d6272a5cc6a57c49da2046165a
Raphael Coelho <raphael_coelho@hotmail.com.br>
2022-08-20 10:54:46 -0300

    fix: fixed menu with links

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,25 @@
 import { SignInButton } from "../SignInButton"
 import styles from "./styles.module.scss"
+import Link from "next/link"
+import { useRouter } from "next/router"
+import { ActiveLink } from "../ActiveLink"
 
 export const Header = () => {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="ig.news" />
-        <nav>
-          <a href="" className={styles.active}>
-            Home
+        <Link href="/">
+          <a>
+            <img src="/images/logo.svg" alt="ig.news" />
           </a>
-          <a href="">Posts</a>
+        </Link>
+        <nav>
+          <ActiveLink href="/" activeClassName={styles.active}>
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink href="/posts" activeClassName={styles.active}>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
         <SignInButton />
       </div>
